39eba35df20758c937b33979cd96a6d42294d3c4
AnitaAccenture <anita.ribar.valah@accenture.com>
2026-04-17 11:08:20 +0000

    fixing Django API endpoint

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -1,6 +1,12 @@
+import { NavLink, Routes, Route } from 'react-router-dom';
+import Activities from './components/Activities';
+import Leaderboard from './components/Leaderboard';
+import Teams from './components/Teams';
+import Users from './components/Users';
+import Workouts from './components/Workouts';
 
 
-import octofitLogo from '../public/octofitapp-small.png';
+import octofitLogo from './assets/octofitapp-small.png';
 
 
 function App() {
